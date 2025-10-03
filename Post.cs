fae63d4f1bfe349dc22aabd25f7a6587b1f8e647
siraj-Eng <wahhajsiraj16@gmail.com>
2025-10-03 09:26:42 +0300

    progress

---
+++ Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post_Simulator
+{
+    public class PostClass
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public int VoteCount { get; private set; }
+
+        public PostClass() 
+        {
+            Title = title;
+            Description = description;
+            DateCreated = DateTime.Now;
+            VoteCount = 0;
+        }
+        
+        //upvote method
+        public void Upvote()
+        {
+            VoteCount++;
+        }
+
+        //downvote
+        public void Downvote()
+        {
+            VoteCount--;
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StackOverflow_Post_Simulator;
+using System;
+
+namespace StackOverflowPostSimulator
+{
+    class program
+    {
+        //Main Method 
+        static void Main(string[] args)
+        {
+            //Created a post from the postclass
+            var post = new PostClass();
+            post._title = "Test";
+            post._description = "I have test tomorrow";
+            post._dateCreated = DateTime.Now;
+            
+            //Adding of the list'
+            Posts.Add(post);
+             
+            //Upvoting Logic
+            if (post.Upvote())
+                post._countValue++;
+            Console.WriteLine("Post upvoted Successfully" +countValue);
+
+            //Downvoting logic
+            if(post.Downvote())
+                post.Downvote();
+            post._countValue--;
+            Console.WriteLine("post downvoted successfully" + _countvalue);
+    
+        }
+    }
+}
