2d094807595c1cd1d6708a773cf190574ccdb6f9
siraj-Eng <wahhajsiraj16@gmail.com>
2025-10-04 10:36:47 +0300

    Added the post simulator

--- Post.cs
+++ Post.cs
@@ -12,6 +12,7 @@ namespace StackOverflow_Post_Simulator
         public string Description { get; private set; }
         public DateTime DateCreated { get; private set; }
         public int VoteCount { get; private set; }
+        public List<AnswerClass> Answers { get; private set; } //List to store the answer
 
         public PostClass( string title, string description) 
         {
@@ -19,9 +20,9 @@ namespace StackOverflow_Post_Simulator
             Description = description;
             DateCreated = DateTime.Now;
             VoteCount = 0;
-        }
+            Answers = new List<AnswerClass>();
 
-       
+        }
 
         //upvote method
         public void Upvote()
@@ -35,14 +36,46 @@ namespace StackOverflow_Post_Simulator
             VoteCount--;
         }
 
+        //Add Answer
+        public void AddAnswer()
+        {
+            Answers.Add(new AnswerClass(answerText));    
+        }
+
         //display
         public void Display()
-        {
-            Console.WriteLine($"\n{Title}");
-            Console.WriteLine($"\n{Description}");
-            Console.WriteLine($"\n{DateCreated.ToShortTimeString}");
-            Console.WriteLine($"\n{VoteCount}");
+        {  
+
+            Console.WriteLine($"\nTitle: {Title}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"DateCreated {DateCreated.ToShortTimeString()}");
+            Console.WriteLine($"VoteCount {VoteCount}");
+            Console.WriteLine($"Answers {Answers.Count}");
+
+            foreach (var ans in Answers) 
+            {
+                ans.Display();  
+            }
         }
         
+    }
+
+    public class AnswerClass
+    {
+        public string AnswerText { get; private set; }
+        public DateTime DateCreated { get; private set; }
+
+        public AnswerClass() 
+        {
+            AnswerText = answerText;
+            DateCreated = DateTime.Now;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"   ↳ {AnswerText} (at {DateCreated.ToShortTimeString()})");
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,19 +10,44 @@ namespace StackOverflowPostSimulator
         {
             var posts = new List<PostClass>();
 
-            //Added post 1
-            var post1 = new PostClass("Test", "I have a test tomorrow");
+            //Get post title from user
+            Console.Write("Enter the post Title");
+            string title1 = Console.ReadLine();
+
+            //Get the post Description
+            Console.WriteLine("Enter the post Description");
+            string description1 = Console.ReadLine();
+
+            var post1 = new PostClass(title1, description1);
             posts.Add(post1);
 
-            var post2 = new PostClass("Error", "Make the error grow");
+            Console.Write("Answer the post Question");
+            string Answer = Console.ReadLine();
+
+            var post2 = new PostClass(Answer); //Object initialization -- feature
+            post2.AddAnswer();
             posts.Add(post2);
 
+            Console.Write("Do you want to upvote or downvote (u/d)");
+            string result = Console.ReadLine();
 
-            //display
-            for (int i = 0; i < posts.Count; i++)
+            if (result.ToLower() == "u")
             {
-                Console.WriteLine("Post Display Loading.....!");
-                Console.WriteLine("Post Display Loaded");
+                post1.Upvote();
+                Console.WriteLine("Post1 upvoted" + post1.VoteCount);
+
+            }
+            else
+            {
+                post1.Downvote();
+                Console.WriteLine("Post1 Downvoted" + post1.VoteCount);
+            }
+
+            //Display
+            for (int i = 0; i < posts.Count; i++) 
+            {
+                var p = posts[i];
+                p.Display();
             }
         }
     }
